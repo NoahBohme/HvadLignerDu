3ed61f7cb8398f66acbc4229bf7ff451f09e0986
NoahBohme <43778295+NoahBohme@users.noreply.github.com>
2021-05-16 14:21:04 +0200

    Update action.php

--- action.php
+++ action.php
@@ -1,10 +1,10 @@
 <?php 
 $navn = $_POST["navn"];
 
-if ($navn == "Alexander" || $navn = "alexander"){
+if ($navn == "Alexander" | $navn = "alexander"){
     echo('Du ligner 100.000');
 }
-else if ($navn == "Emil" | $navn == "Noah" || $navn == "emil" || $navn == "noah"){
+else if ($navn == "Emil" | $navn == "Noah" | $navn == "emil" | $navn == "noah"){
     echo('Du ligner en 1.000.000');
 }
 else {
