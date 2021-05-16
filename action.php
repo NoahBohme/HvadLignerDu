5b3fb9efecda27a3c87061641c32633bde386708
NoahBohme <43778295+NoahBohme@users.noreply.github.com>
2021-05-16 14:24:42 +0200

    Change to or

--- action.php
+++ action.php
@@ -1,10 +1,10 @@
 <?php 
 $navn = $_POST["navn"];
 
-if ($navn == "Alexander" | $navn = "alexander"){
+if ($navn == "Alexander" or $navn = "alexander"){
     echo('Du ligner 100.000');
 }
-else if ($navn == "Emil" | $navn == "Noah" | $navn == "emil" | $navn == "noah"){
+else if ($navn == "Emil" or $navn == "Noah" or $navn == "emil" or $navn == "noah"){
     echo('Du ligner en 1.000.000');
 }
 else {
