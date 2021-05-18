bb11dbefdc028ae6f4871401919660432ba47bfd
NoahBohme <43778295+NoahBohme@users.noreply.github.com>
2021-05-18 17:45:41 +0200

    Update action.php
    Virklighedsopdatering

--- action.php
+++ action.php
@@ -1,8 +1,11 @@
 <?php 
 $navn = $_POST["navn"];
 
-if ($navn == "Alexander" or $navn = "alexander" || $navn == "Emil" or $navn == "Noah" or $navn == "emil" or $navn == "noah"){
+if ($navn == "Emil" or $navn == "Noah" or $navn == "emil" or $navn == "noah"){
     echo('Du ligner en 1.000.000');
+}
+if ($navn == "Alexander" or $navn = "alexander"){
+    echo('Du ligner en 100.000');
 }
 else {
     echo('Du ligner pis');
